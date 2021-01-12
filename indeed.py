f5f38d48e4f32ada40ff171504f1b2171f1318da
moondancer92 <70056397+moondancer92@users.noreply.github.com>
2021-01-12 16:27:53 +0900

    project_started
    project statred
    
    created extract page number
    extract job title,company,lcoation,URL

---
+++ indeed.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+URL = f"https://jp.indeed.com/%E6%B1%82%E4%BA%BA?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=%E6%9D%B1%E4%BA%AC%E9%83%BD&fromage=any&limit={LIMIT}"
+
+
+def extract_indeed_pages():
+    result = requests.get(URL)
+    soup = BeautifulSoup(
+        result.text, 'html.parser')
+    pagination = soup.find(
+        "div", {"class": "pagination"})
+    links = pagination.find_all('a')
+    pages = []
+    for link in links[:-1]:
+        pages.append(int(link.string))
+    last_page = (pages[-1])
+    return last_page
+
+
+def extract_job(html):
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = (str(company.find("a").string))
+    else:
+        company = (str(company.string))
+    company = company.strip()
+    location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return{'title': title, "company": company, "location": location,
+           "link": f"https://jp.indeed.com/viewjob?jk={job_id}"}
+
+
+def extract_indeed_jobs(max_page):
+    jobs = []
+    for page in range(max_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
